9c0c5a25daa1956338b8c316a0b9fff87f7235ca
Adithya2310 <Adithyanandi099@gmail.com>
2023-04-16 01:32:43 +0530

    completed project

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React,{useState} from 'react'
+import "./App.css"
+import useClipboard from "react-use-clipboard";
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+
+
+
+const App = () => {
+
+  const {
+    transcript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
+
+  const [copyText,setCopyText]=useState('');
+  const [isCopied, setCopied] = useClipboard(copyText,{
+    successDuration:1000
+  });
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
+  return (
+    <div>
+      <div className="container">
+        <h2>Speach to Text Converter</h2>
+        <p>An WebApp that provides users with the ability to convert spoken words into written text with ease. This app is designed to allow users to speak into a microphone, and the app will transcribe their speech into text format in real-time.</p>
+        <div className="main-content" onClick={()=>setCopyText(transcript)}>
+          {transcript}
+        </div>
+        <div className="btn-style">
+        <button onClick={setCopied}>
+           {isCopied ? "Copy to clipboard" : "Copied"}
+        </button>
+        <button onClick={()=>SpeechRecognition.startListening({continuous:true,language:'en-IN'})}>StartListening</button>
+        <button onClick={SpeechRecognition.stopListening}>StopListening</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
